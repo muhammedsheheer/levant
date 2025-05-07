bfcb889e4639c232c2a6d262386e09f0d03261bb
sheheer <muhammedsheheer99@gmail.com>
2025-05-07 18:13:36 +0530

    updated booking

--- src/app/table-booking/(section)/Booking.tsx
+++ src/app/table-booking/(section)/Booking.tsx
@@ -5,8 +5,15 @@ const Booking: React.FC = () => {
   return (
     <div className="flex h-full w-full max-w-[1300px] items-center justify-center gap-[1.2rem] py-12">
       <iframe
-        src="https://tableagent.com/iframe/levant-restaurant/v/medium/"
-        style={{ width: "100%", height: "620px" }}
+        src="https://tableagent.com/iframe/levant-restaurant/"
+        style={{
+          border: "0px none",
+          minWidth: "375px",
+          minHeight: "736px",
+        }}
+        sandbox="allow-forms allow-modals allow-same-origin allow-scripts allow-top-navigation-by-user-activation"
+        width="100%"
+        height="100%"
       ></iframe>
     </div>
   );
