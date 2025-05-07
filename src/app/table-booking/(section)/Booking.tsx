02b8c012ac4ea3054f3452b1e6eee770ca5902dd
MOHAMMED SHEHEER C B <156364923+muhammedsheheer@users.noreply.github.com>
2025-05-07 17:10:21 +0530

    Update Booking.tsx

--- src/app/table-booking/(section)/Booking.tsx
+++ src/app/table-booking/(section)/Booking.tsx
@@ -1,20 +1,13 @@
-import Link from "next/link";
-import React from "react";
+"use client";
 
-const Booking: React.FC = () => {
+const Booking = ({}) => {
   return (
-    <iframe
-      src="https://tableagent.com/iframe/levant-restaurant/v/medium/"
-      style={{
-        border: "0px none",
-        minWidth: "300px",
-        maxWidth: "350px",
-        minHeight: "636px",
-      }}
-      sandbox="allow-forms allow-modals allow-same-origin allow-scripts allow-top-navigation-by-user-activation"
-      width="100%"
-      height="100%"
-    ></iframe>
+    <div className="flex h-full w-full max-w-[1300px] items-center justify-center gap-[1.2rem] py-12">
+      <iframe
+        src="https://tableagent.com/iframe/levant-restaurant/v/medium/"
+        style={{ width: "100%", height: "620px" }}
+      ></iframe>
+    </div>
   );
 };
 
